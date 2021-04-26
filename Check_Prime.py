bef1ec945ab4fdf9fba6755086079ae7befa279c
Ajay Kumar Nagar <anagarjnv889@gmail.com>
2021-04-26 22:32:07 +0530

    292020

--- Check_Prime.py
+++ Check_Prime.py
@@ -1,14 +1,12 @@
 n=int(input("Enter the number\n"))
 def prime(n):
-    po=int(n**0.5)
     if(n<0):
         print("Enter a positive number.")
     elif(n==0 or n==1):
         print("Neither prime nor composite.")
     else:
-        for i in range(2,po+1):
+        for i in range(2,n):
             if n%i==0:
-                return False;
+                return False
     return True
-
 print(prime(n))
