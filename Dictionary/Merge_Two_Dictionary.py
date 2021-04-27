b9d107d6babee481d6bbf6bc3fd1a50d6925f27d
Ajay Kumar Nagar <anagarjnv889@gmail.com>
2021-04-27 14:48:02 +0530

    292020 update dictionary

---
+++ Dictionary/Merge_Two_Dictionary.py
@@ -0,0 +1,6 @@
+dict1={'BMW':2500,'Apachy':1000,'Ninjas':3000,}
+dict2={'RR310':1555,'Hero':1200}
+print("Dictionary one is \n",dict1)
+print("Dictionary two is \n",dict2)
+dict1.update(dict2)
+print("Dictionary one is after merge \n",dict1)
