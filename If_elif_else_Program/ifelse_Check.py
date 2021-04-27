1cf85a45754bf9e2808144a68001e1e8cf5db8ef
Ajay Kumar Nagar <anagarjnv889@gmail.com>
2021-04-27 11:55:34 +0530

    292020 update if

---
+++ If_elif_else_Program/ifelse_Check.py
@@ -0,0 +1,9 @@
+n=input("Enter the input\n")
+if n==0:
+    print("It is zero")
+
+
+check=isinstance(n,float)
+print("Given input is float ",check)
+check1=isinstance(n,int)
+print("Given input is int ",check1)
