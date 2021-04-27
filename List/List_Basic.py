ae922ecd6cb44a1d8ca7acb8420584268d882b14
Ajay Kumar Nagar <anagarjnv889@gmail.com>
2021-04-27 12:06:27 +0530

    292020 update list

---
+++ List/List_Basic.py
@@ -0,0 +1,8 @@
+list=[1,45,8,56,45]
+print("Original list ",list)
+list.append(10)
+print("List after append ",list)
+list.insert(0,200)
+print("List after insert",list)
+del list[2]
+print("List after delete ",list)
