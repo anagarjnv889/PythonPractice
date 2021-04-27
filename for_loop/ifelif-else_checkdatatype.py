db32309265602bf3fce772db1515c579459c31d2
Ajay Kumar Nagar <anagarjnv889@gmail.com>
2021-04-27 11:55:48 +0530

    292020 update delete

--- for_loop/ifelif-else_checkdatatype.py
+++
@@ -1,9 +0,0 @@
-n=input("Enter the input\n")
-if n==0:
-    print("It is zero")
-
-
-check=isinstance(n,float)
-print("Given input is float ",check)
-check1=isinstance(n,int)
-print("Given input is int ",check1)
